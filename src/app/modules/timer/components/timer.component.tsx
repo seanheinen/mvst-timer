6fa403c1dacb3ae702a9240cca02d8033a8aa067
Sean Heinen <seanheinen88@gmail.com>
2021-08-06 17:15:20 +0300

    Create header with theme toggle

---
+++ src/app/modules/timer/components/timer.component.tsx
@@ -0,0 +1,16 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+
+export class TimerComponent extends React.Component {
+
+  render() {
+    return (
+      <Grid container>
+        <Grid item alignContent="center">
+
+        </Grid>
+
+      </Grid>
+    );
+  }
+}
